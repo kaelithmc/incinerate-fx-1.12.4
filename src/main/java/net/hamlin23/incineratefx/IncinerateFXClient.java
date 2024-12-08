091070c6a29c8aafab9001a2b74fd54a190d0832
Jae Evans <jacobaiden07@gmail.com>
2024-12-08 12:55:14 -0600

    Initial commit

---
+++ src/main/java/net/hamlin23/incineratefx/IncinerateFXClient.java
@@ -0,0 +1,10 @@
+package net.hamlin23.incineratefx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IncinerateFXClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hamlin23/incineratefx/IncinerateFXDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hamlin23.incineratefx;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IncinerateFXDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
